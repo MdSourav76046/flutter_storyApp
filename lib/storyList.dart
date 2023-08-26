489409d3978e6961bbba83e43e445758de003aa2
MdSourav76046 <adibamama807@gmail.com>
2023-08-26 16:51:27 +0600

    new added

---
+++ lib/storyList.dart
@@ -0,0 +1,19 @@
+import 'dart:core';
+
+import 'package:flutter/material.dart';
+
+
+List<String> allStory = [
+  "Hello guys, myself majid Bhuiya, Hazaro meyeder crush. If You love me then call me. LOve yaa, ummah",
+  "Ami jani tmi k tmi jano ami k, cholo giye moja kori. come on baby.",
+];
+
+List<String> allTitle = [
+  "Lust Story of Majid",
+  "Story of A Legend!",
+];
+
+List<String> allImageLink = [
+  "assets/majid.jpeg",
+  "assets/ami_tmi.jpg",
+];
